d0dbbeb7263a1cf505074a2b267266b2a5cb2cdb
Chance Linz <clinz.dev@gmail.com>
2024-08-19 20:53:49 -0700

    moving items around in go list command output

--- cmd/list.go
+++ cmd/list.go
@@ -35,14 +35,14 @@ func executeList(cmd *cobra.Command, args []string) error {
 	// Create a new tabwriter
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', tabwriter.DiscardEmptyColumns)
 
-	headers := []string{"TASK. NO", "ID", "NAME", "STATUS", "NOTES", "CREATED ON", "COMPLETED ON"}
+	headers := []string{"TASK. NO", "ID", "NAME", "NOTES", "STATUS", "CREATED ON", "COMPLETED ON"}
 	header := strings.Join(headers, "\t")
 
 	// Print table header
 	fmt.Fprintln(w, header)
 
 	for index, item := range items {
-		fmt.Fprintf(w, "%d\t%s\t%s\t%s\t%s\t%s\t%s\t\n", index+1, item.ID, item.Name, item.Status, item.Notes, item.CreatedOn, item.CompletedOn)
+		fmt.Fprintf(w, "(%d)\t%s\t%s\t%s\t%s\t%s\t%s\t\n", index+1, item.ID, item.Name, item.Notes, item.Status, item.CreatedOn, item.CompletedOn)
 	}
 
 	w.Flush()
--- internal/model/item.go
+++ internal/model/item.go
@@ -1,10 +1,10 @@
 package model
 
 type GodoItem struct {
-	ID          string
-	Name        string
-	Notes       string
-	Status      string
-	CompletedOn string
-	CreatedOn   string
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Notes       string `json:"notes"`
+	Status      string `json:"status"`
+	CompletedOn string `json:"completed_on"`
+	CreatedOn   string `json:"created_on"`
 }
